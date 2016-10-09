29e2aa726de92372c72c293a88127186a0e4358b
Bob <me@loc.al>
2016-10-09 23:11:51 +0300

    Integrated VK SDK

--- app/src/main/java/ru/coolsoft/vkfriends/MainActivity.java
+++ app/src/main/java/ru/coolsoft/vkfriends/MainActivity.java
@@ -1,24 +1,43 @@
 package ru.coolsoft.vkfriends;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+//import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.TypedValue;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Space;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKApiUser;
+import com.vk.sdk.api.model.VKList;
 
 public class MainActivity extends AppCompatActivity
 implements AppBarLayout.OnOffsetChangedListener
-, NavigationView.OnNavigationItemSelectedListener{
+, NavigationView.OnNavigationItemSelectedListener, SharedPreferences.OnSharedPreferenceChangeListener {
+    private final static String TAG = MainActivity.class.getSimpleName();
 
+    //main view controls
     private FrameLayout mFl;
     private TextView mContactNameLeft;
     private TextView mContactNameRight;
@@ -27,9 +46,78 @@ implements AppBarLayout.OnOffsetChangedListener
     private Space mSpace1;
     private Space mSpace2;
 
+    //measuring paramenters
     private int mTextSizeStart;
     private int mTextSizeEnd;
     private int mAppBarHeight;
+
+    //navigation controls
+    private NavigationView mNavView;
+    private ProgressBar mWaiter;
+
+    //callback handlers
+    //Handler mDelayHandler = new Handler();
+    private VKCallback<VKAccessToken> mVKAuthCallback = new VKCallback<VKAccessToken>() {
+        @Override
+        public void onResult(VKAccessToken res) {
+            // Пользователь успешно авторизовался
+            res.saveTokenToSharedPreferences(MainActivity.this, VKFApplication.PREF_KEY_ACCESS_TOKEN);
+            //reload avatar
+            //update contacts in db
+        }
+        @Override
+        public void onError(VKError error) {
+            // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+        }
+    };
+    private VKRequest.VKRequestListener mVKCurrentUserRequestListener = new VKRequest.VKRequestListener() {
+        @Override
+        public void onComplete(VKResponse response) {
+            Log.d(TAG, "USER data retrieved");
+
+            if (response.parsedModel instanceof VKList){
+                Object o = ((VKList)response.parsedModel).get(0);
+                if (o instanceof VKApiUser){
+                    VKApiUser me = (VKApiUser) o;
+                    final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                    final String name = sp.getString(VKFApplication.PREF_KEY_USERNAME, null);
+                    final String newName = me.toString();
+
+                    /*===DEBUG DELAY===*/
+                    /**
+                    final boolean posted = mDelayHandler.postDelayed(
+                        new Runnable() {
+                            @Override
+                            public void run() {/**/
+                                Log.d(TAG, "Updating USERNAME preference");
+                                if (newName.equals(name)){
+                                    refreshNavigationView();
+                                } else {
+                                    SharedPreferences.Editor editor = sp.edit();
+                                    editor.putString(VKFApplication.PREF_KEY_USERNAME, newName);
+                                    editor.apply();
+                                }
+                                mWaiter.setVisibility(View.GONE);
+                            /**}
+                        }
+                        , 5000
+                    );/**/
+                    //Log.d(TAG, "update posted = " + posted);
+                }
+            }
+
+            super.onComplete(response);
+        }
+
+        @Override
+        public void onError(VKError error) {
+            Log.d(TAG, "Error retrieving USER data: " + error.errorMessage);
+            refreshNavigationView();
+            mWaiter.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            super.onError(error);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +125,20 @@ implements AppBarLayout.OnOffsetChangedListener
         setContentView(R.layout.activity_main);
 
         initParams();
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+        initViews();
+    }
 
+    private void initParams(){
+        TypedArray attr = getTheme().obtainStyledAttributes(new int[]{android.R.attr.actionBarSize});
+        mAppBarHeight = attr.getDimensionPixelSize(0, 0);
+        attr.recycle();
+
+        mTextSizeStart = getResources().getDimensionPixelSize(R.dimen.text_size);
+        mTextSizeEnd = getResources().getDimensionPixelSize(R.dimen.text_size_small);
+    }
+
+    private void initViews() {
         ImageView anchor = (ImageView) findViewById(R.id.padding);
         if (anchor!= null) {
             ViewGroup.LayoutParams lp = anchor.getLayoutParams();
@@ -49,10 +150,18 @@ implements AppBarLayout.OnOffsetChangedListener
             appBar.addOnOffsetChangedListener(this);
         }
 
-        NavigationView nav = (NavigationView) findViewById(R.id.navigation);
-        if(nav != null) {
-            nav.setNavigationItemSelectedListener(this);
+        mNavView = (NavigationView) findViewById(R.id.navigation);
+        mWaiter = (ProgressBar) findViewById(R.id.waiter);
+        if(mNavView != null) {
+            mNavView.setNavigationItemSelectedListener(this);
+            if (VKFApplication.app().isInitialized()){
+                refreshNavigationView();
+            } else {
+                requestAccountDetails();
+                VKFApplication.app().setInitialized();
+            }
         }
+
         mFl = (FrameLayout) findViewById(R.id.title);
 
         mContactNameLeft = (TextView) findViewById(R.id.name1);
@@ -61,15 +170,6 @@ implements AppBarLayout.OnOffsetChangedListener
         mContactImageRight = (ImageView) findViewById(R.id.avatar2);
         mSpace1 = (Space) findViewById(R.id.spaceAvatar1);
         mSpace2 = (Space) findViewById(R.id.spaceAvatar2);
-    }
-
-    private void initParams(){
-        TypedArray attr = getTheme().obtainStyledAttributes(new int[]{android.R.attr.actionBarSize});
-        mAppBarHeight = attr.getDimensionPixelSize(0, 0);
-        attr.recycle();
-
-        mTextSizeStart = getResources().getDimensionPixelSize(R.dimen.text_size);
-        mTextSizeEnd = getResources().getDimensionPixelSize(R.dimen.text_size_small);
     }
 
     @Override
@@ -118,11 +218,82 @@ implements AppBarLayout.OnOffsetChangedListener
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
+        switch (id){
+            case R.id.action_settings:
+
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_login:
+                VKSdk.login(this, VKScope.FRIENDS);
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, mVKAuthCallback)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch(key){
+            case VKFApplication.PREF_KEY_ACCESS_TOKEN:
+                onTokenUpdated();
+                break;
+            case VKFApplication.PREF_KEY_USERNAME:
+                refreshNavigationView();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Changes User Name shared preference accordingly that will cause {@link #refreshNavigationView()} to be invoked
+     */
+    private void requestAccountDetails(){
+        Log.d(TAG, "Access Token update handler");
+        final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        final VKAccessToken token = VKAccessToken.tokenFromSharedPreferences(this, VKFApplication.PREF_KEY_ACCESS_TOKEN);
+        if (token == null){
+            final String name = sp.getString(VKFApplication.PREF_KEY_USERNAME, null);
+            if (name != null){
+                Log.d(TAG, "removing USERNAME preference");
+                SharedPreferences.Editor editor = sp.edit();
+                editor.remove(VKFApplication.PREF_KEY_USERNAME);
+                editor.apply();
+            }
+        } else {
+            mWaiter.setVisibility(View.VISIBLE);
+
+            VKRequest request = VKApi.users().get();
+            Log.d(TAG, "requesting USER data");
+            request.executeWithListener(mVKCurrentUserRequestListener);
+        }
+    }
+
+    private void onTokenUpdated(){
+        requestAccountDetails();
+    }
+
+    /**
+     * Invoked when change of access token requests update of the user name
+     * or the activity wants to refresh the user name in its Navigation View
+     */
+    private void refreshNavigationView(){
+        final MenuItem miLogin = mNavView.getMenu().findItem(R.id.action_login);
+        final String name = PreferenceManager.getDefaultSharedPreferences(this).getString(VKFApplication.PREF_KEY_USERNAME, null);
+
+        //ToDo: update user image as well
+        Log.d(TAG, "Updating user name in Navigation View. Current name is " + String.valueOf(name));
+        if (name == null) {
+            miLogin.setTitle(R.string.action_login);
+        } else {
+            miLogin.setTitle(name);
+        }
     }
 }
---
+++ app/src/main/java/ru/coolsoft/vkfriends/VKFApplication.java
@@ -0,0 +1,58 @@
+package ru.coolsoft.vkfriends;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
+import com.vk.sdk.VKSdk;
+
+/**
+ * Created by Bobby© on 05.10.2016.
+ * The application class that stores global data and contains general watchdogs
+ */
+public class VKFApplication extends Application {
+    private static final String TAG = VKFApplication.class.getSimpleName();
+
+    public static final String PREF_KEY_ACCESS_TOKEN = "access_token";
+    public static final String PREF_KEY_USERNAME = "user_name";
+
+    private static VKFApplication mApp;
+
+    private boolean mInitialized;
+    private VKAccessTokenTracker mVKAccessTokenTracker = new VKAccessTokenTracker() {
+        @Override
+        public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
+            Log.d(TAG, "Auth Token has just changed from " + String.valueOf(oldToken) + " to " + String.valueOf(newToken));
+            if (newToken == null) {
+                // VKAccessToken is invalid
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(VKFApplication.this).edit();
+                editor.remove(PREF_KEY_ACCESS_TOKEN);
+                editor.apply();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mApp = this;
+
+        mVKAccessTokenTracker.startTracking();
+        VKSdk.initialize(this);
+    }
+
+    public static VKFApplication app(){
+        return mApp;
+    }
+
+    public boolean isInitialized(){
+        return mInitialized;
+    }
+
+    public void setInitialized(){
+        mInitialized = true;
+    }
+}
